fff344e35408982d46bd2beed62a1e252e90e5e0
Saurav Shekhar <weblife.shekhar24@gmail.com>
2021-09-03 18:48:01 +0530

    Update issuance_redemption.sol

--- issuance_redemption.sol
+++ issuance_redemption.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.6.10;
@@ -30,6 +29,7 @@ contract IssueSET {
     address public maticAToken = 0x720ddAa73C16B12b5AB6362BacCB504cFd907121 ;
     address public maticBToken = 0x425783dE6E1C57903eD5eb457208eCB9D224965C ;
     address public maticsetTokenPABaddress = 0x0cBD78846F2F63401639e36905b626f136E1ce39;
+    IBasicIssuanceModule issuance;
     
     address public basicIssuanceModuleAddress ;
    	 
@@ -42,12 +42,13 @@ contract IssueSET {
 	   constructor(address _issuance ) public {
 	       
 	        basicIssuanceModuleAddress = _issuance;
+	        issuance = IBasicIssuanceModule(basicIssuanceModuleAddress);
 	        
 	        
 	  }
 	   
 	 
-	  IBasicIssuanceModule issuance = IBasicIssuanceModule(basicIssuanceModuleAddress);
+	  
    
 
  /////////////////////////////////////////////////////////////////// BASIC ISSUANCE  RELATED FUNCTIONS/////////////////////////////////////////////////////////
@@ -68,16 +69,14 @@ contract IssueSET {
 		//Here msg.sender is my wallet account address "0x3793f758a36c04B51a520a59520e4d845f94F9F2" which holds MTOA and MTOB Tokens
 		//oh and you'll also need to approve your contract to transfer tokens from your wallet
 		
-        IERC20(maticAToken).approve(address(this) ,8000000000000000000);
-        IERC20(maticAToken).transferFrom( msg.sender ,address(this) ,7000000000000000000);
+        IERC20(maticAToken).transferFrom( msg.sender ,address(this) ,7000000000000000000000);
           
-        IERC20(maticBToken).approve(address(this) ,8000000000000000000);
-        IERC20(maticBToken).transferFrom( msg.sender ,address(this) ,7000000000000000000);
+        IERC20(maticBToken).transferFrom( msg.sender ,address(this) ,7000000000000000000000);
 		
 //then your contract needs to do an approval to the BasicIssuanceModule for those amounts and 
 		
-		IERC20(maticAToken).approve(address(issuance) ,6000000000000000000);
-		IERC20(maticBToken).approve(address(issuance) ,6000000000000000000);
+		IERC20(maticAToken).approve(address(issuance) ,6000000000000000000000);
+		IERC20(maticBToken).approve(address(issuance) ,6000000000000000000000);
 		   
 		
 		  //then call the issue on the BasicIssuanceModule
@@ -90,10 +89,31 @@ contract IssueSET {
  	 }
 	 
 
+
+
+	
+	function redeemSET2 ( ISetToken _setToken, uint256 _quantity, address _to) external {
+	   
+   
+		// Here 1 PAB SET Token Consists of 5000 MTOA Token & 2000 MTOB Underlying Token
+	    
+     ISetToken mySet = ISetToken(_setToken);
+     uint256 quantity = _quantity;
+     address to = _to; 
+          
+    IERC20(maticsetTokenPABaddress).transferFrom( msg.sender ,address(this) , quantity);
+
+    issuance.redeem( mySet , quantity, to); 
+   
+
+                 
+ 	 }
+	 
+
 	
 	
 	 
-	 	 function getRequiredComponentUnitsForIssueData (ISetToken _mySet , uint256 _quantity) external returns (address[] memory, uint256[] memory) {
+	 	 function getRequiredComponentUnitsForIssueData (ISetToken _mySet , uint256 _quantity) external view returns (address[] memory, uint256[] memory) {
 	 	     
 	 	    
 	 	      ISetToken mySet = ISetToken(_mySet);
@@ -134,7 +154,20 @@ contract IssueSET {
 		   
 		   return  mySets.getComponents();
 	   }
-	 
+	   
+	   
+
+
+	  function addModules( address _setToken ,  address _modules) external {
+	
+		(bool success ,bytes memory data) =  _setToken.delegatecall(abi.encodeWithSignature("addModule(address)" , _modules));
+		  
+	      //mySets.addModule(_modules);
+	
+} 
+
+
+
     
 }
 
